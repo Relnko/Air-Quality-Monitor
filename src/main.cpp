acecbaf08395f77b93bbe4eb8b3d186c2bdea8e1
Rango <reinko.rothert@outlook.de>
2026-04-19 13:54:26 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include <XPT2046_Touchscreen.h>
+#include <SensirionI2cScd4x.h>
+
+void setup() {
+}
+
+void loop() {
+}
