cd734bbb7215dd95915b5db66a4413358fc74bf5
Rango <reinko.rothert@outlook.de>
2026-04-21 13:03:48 +0200

    Added common.h for global values and pinouts

---
+++ lib/Common/common.h
@@ -0,0 +1,38 @@
+#ifndef common_H_
+#define common_H_
+
+#define TFT_DC 9
+#define TFT_CS 10
+#define TFT_RST 8
+#define Touch_CS 7
+#define Touch_IRQ 2
+
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 240
+
+/*
+0--------X------320, 0
+|
+Y
+|
+0, 240
+*/
+
+#define TOUCH_MIN_X 240
+#define TOUCH_MAX_X 3700
+#define TOUCH_MIN_Y 280
+#define TOUCH_MAX_Y 3800
+
+#define BUTTON_WIDTH 40
+#define BUTTON_HEIGHT 40
+#define BUTTON_X 270 // Darkmode + Exit
+#define BUTTON_Y 190 // Darkmode + AllSensorScreen + SystemHealth
+#define BUTTONallsens_X 10 // AllSensorScreen
+#define BUTTONexit_Y 10 // Exit
+#define BUTTONsyshealth_X 140 // SystemHealth
+
+extern int Foregroundcolor;
+extern int Backgroundcolor;
+extern bool Darkmode;
+
+#endif // common_H_
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Adafruit_ILI9341.h>
 #include <XPT2046_Touchscreen.h>
 #include <SensirionI2cScd4x.h>
+#include <common.h>
 
 void setup() {
 }
